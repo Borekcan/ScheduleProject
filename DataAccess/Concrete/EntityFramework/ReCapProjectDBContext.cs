320676b53e72fea29654357862558fe00f3983f7
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-12-08 23:07:33 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/ReCapProjectDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Concrete;
+using Core.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+   public class ReCapProjectDBContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DOĞUKAN\SQL2;Database=ReCapProjectDB;Trusted_Connection=true");
+        }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    }
+}
